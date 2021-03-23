d57a49dde79e2b22811ed454d424c1035cf58acf
HTMLProjectMaster <74315691+HTMLProjectMaster@users.noreply.github.com>
2021-03-23 13:17:19 +0100

    Importing the project
    First commit, importation of the engine (previously named js-canvas-base)

---
+++ modules/library.js
@@ -0,0 +1,6 @@
+/* root and core modules are declared in index.html */
+// modules
+system.load('script', 'script-input', 'modules/input.js', system.dom.head)
+system.load('script', 'script-item', 'modules/item.js', system.dom.head)
+system.load('script', 'script-scenic', '../scenic.js', system.dom.head)
+
---
+++ modules/util.js
@@ -0,0 +1,16 @@
+let util = {	
+	sleep: function(tick){
+		return new Promise(resolve => setTimeout(resolve, tick))
+	},
+	rng: function(max){
+		return Math.floor(Math.random() * Math.floor(max))
+	},
+	line: function(x1, y1, x2, y2, color){
+		canvas.context.beginPath()
+		canvas.context.strokeStyle = color
+		canvas.context.moveTo(x1, y1)
+		canvas.context.lineTo(x2, y2)	
+		canvas.context.stroke()
+		canvas.context.closePath()
+	}	
+}
