618c215d985470c9c9017835d89e0827625d100c
HTMLProjectMaster <74315691+HTMLProjectMaster@users.noreply.github.com>
2021-03-29 01:40:55 +0200

    Updating util
    Adding collision function:
     - AABB trigger system
     - Vectoryzing objects

--- modules/util.js
+++ modules/util.js
@@ -12,5 +12,16 @@ let util = {
 		canvas.context.lineTo(x2, y2)	
 		canvas.context.stroke()
 		canvas.context.closePath()
-	}	
+	},
+	collide: function(x1, y1, scale1, x2, y2, scale2){
+		if (x1 < x2 + scale2 &&
+			x1 + scale1 > x2 &&
+			y1 < y2 + scale2 &&
+			scale1 + y1 > y2) {
+				return true
+		}
+	},
+	vectoryze: function(x1, y1, x2, y2){
+		return {x: x1 - x2, y: y1 - y2}
+	}
 }
